c54400f19d301b193ee78fd46a0bd47783cbe671
creekroadpottery <creekroadpottery@gmail.com>
2025-09-03 22:37:47 -0400

    Update pottery_pricing_app.py

--- pottery_pricing_app.py
+++ pottery_pricing_app.py
@@ -328,8 +328,8 @@ with tabs[0]:
     left, right = st.columns(2)
 
     # left column
-    with left:
-# --- Form preset picker (with options) ---
+with left:
+  # --- Form preset picker (with options) ---
     st.subheader("Form preset")
 
     preset_df = ensure_cols(
