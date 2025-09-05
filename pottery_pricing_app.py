2445f0998c61c0b67385d55d29ddcac718711c64
creekroadpottery <creekroadpottery@gmail.com>
2025-09-05 15:25:07 -0400

    Update pottery_pricing_app.py

--- pottery_pricing_app.py
+++ pottery_pricing_app.py
@@ -288,7 +288,8 @@ with tabs[0]:
     left, right = st.columns(2)
 
     # ---------------- LEFT ----------------
-    with left:
+    
+with left:
         # --- Form preset picker (with apply + manager) ---
 st.subheader("Form preset")
 
